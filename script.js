e5fec7d93431490195504c04effc64f9b6af355f
kalkinamrath <kalkinamrath@gmail.com>
2026-02-26 19:40:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function openGame(game) {
+
+    if (game === "snake") {
+        window.location.href = "games/snake.html";
+    }
+
+    if (game === "tictactoe") {
+        window.location.href = "games/tictactoe.html";
+    }
+
+    if (game === "cowsbulls") {
+        window.location.href = "games/cowsbulls.html";
+    }
+}
